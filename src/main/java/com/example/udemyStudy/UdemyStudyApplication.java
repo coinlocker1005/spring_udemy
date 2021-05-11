cbf5d2460ba08f1c1b8907945c08cb19f58a41fd
itoshuhei <ito.shuhei@tis.co.jp>
2021-05-11 22:22:25 +0900

    first commit

---
+++ src/main/java/com/example/udemyStudy/UdemyStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemyStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/udemyStudy/UdemyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udemyStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
